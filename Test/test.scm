17f2b0a2a4e4eb3b729a68b7ef417f70fa721aaf
Richard Frith-MacDonald <rfm@gnu.org>
1998-11-30 10:27:04 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/guile/trunk@3356 72102866-910b-0410-8b05-ffd578937521

--- Test/test.scm
+++ Test/test.scm
@@ -3,6 +3,7 @@
 (use-modules (ice-9 greg))
 (display "\nRunning basic tests...\n\n")
 (set! greg-tools (list "tests"))
+(set! greg-verbose 4)
 (greg-test-run)
 (display "\nTests complete.\n\n")
 (display "\nThere should be a log in 'tests.log'\n")
