ddd89e7db6cab0cedf839111cb8f9af2a4e589de
richard <richard@72102866-910b-0410-8b05-ffd578937521>
2000-03-12 09:37:21 +0000

    Updates for 1.1 release
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/guile/trunk@6275 72102866-910b-0410-8b05-ffd578937521

--- Greg/Test/tests/greg.scm
+++ Greg/Test/tests/greg.scm
@@ -1,24 +1,25 @@
 
 ;   Copyright (C) 1998 Free Software Foundation, Inc.
 ;
-;   Written by:  Richard Frith-Macdonald <richard@brainstorm.co.uk>
+;   Written by:  Richard Frith-Macdonald <rfm@gnu.org>
 ;   Date: April 1998
 ;
-;   This file is part of the Greg package - part of the GNUstep project.
+; This file is part of the Greg package - part of the GNUstep project.
 ;
-;   This library is free software; you can redistribute it and/or
-;   modify it under the terms of the GNU Library General Public
-;   License as published by the Free Software Foundation; either
-;   version 2 of the License, or (at your option) any later version.
+; This library is free software; you can redistribute it and/or
+; modify it under the terms of the GNU Lesser General Public
+; License as published by the Free Software Foundation; either
+; version 2.1 of the License, or (at your option) any later version.
+; 
+; This library is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+; Lesser General Public License for more details.
+; 
+; You should have received a copy of the GNU Lesser General Public
+; License along with this library; if not, write to the Free Software
+; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ;
-;   This library is distributed in the hope that it will be useful,
-;   but WITHOUT ANY WARRANTY; without even the implied warranty of
-;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;   Library General Public License for more details.
-;
-;   You should have received a copy of the GNU Library General Public
-;   License along with this library; if not, write to the Free
-;   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 (greg-testcase "We can return an expected pass" #t
 (lambda ()
