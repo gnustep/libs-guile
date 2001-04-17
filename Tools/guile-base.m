d98e8f841d1245407d565a5ca59309a592f18076
nico <nico@72102866-910b-0410-8b05-ffd578937521>
2001-04-17 14:58:38 +0000

    Imported with modifications from the tests module
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/guile/trunk@9602 72102866-910b-0410-8b05-ffd578937521

---
+++ Tools/guile-base.m
@@ -0,0 +1,23 @@
+/*
+ *	A trivial replacement for the Guile shell - but with the Objective-C
+ *	runtime and the base library linked in so that we can test things
+ */
+
+#include <gstep_guile.h>
+#include <guile/gh.h>
+
+static void
+inner_main(void* colsure, int argc, char **argv)
+{
+    gstep_init();
+    gstep_link_base();
+    scm_shell(argc, argv);
+}
+
+int
+main(int argc, char** argv)
+{
+  scm_boot_guile (argc, argv, inner_main, 0);
+  return 0;
+}
+
